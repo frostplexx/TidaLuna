da05e9eb40050ca6f9ecefaab0ae97a034366e5c
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-02-22 13:17:28 +0100

    test

--- nix/injection.nix
+++ nix/injection.nix
@@ -8,13 +8,33 @@
 }:
 let
   package = builtins.fromJSON (builtins.readFile ../package.json);
-in
-stdenv.mkDerivation (rec {
+
   name = "TidaLuna";
   pname = "${name}";
-
-  version = package.version;
   src = ./..;
+  inherit (package) version;
+
+  nixOSDeps = fetchPnpmDeps {
+    inherit pname src version;
+    fetcherVersion = 1;
+    hash = "sha256-Oj34rQbKbsHnqPdVv+ti8z+gZTT+VOsDxg/MQ22sLRQ=";
+  };
+
+  darwinDeps = fetchPnpmDeps {
+    inherit pname src version;
+    fetcherVersion = 1;
+    hash = "sha256-pHIY4Ie66ZVwEne/4RmY2QvsRWcnfsl2kv3CDXcqVrg=";
+  };
+
+in
+stdenv.mkDerivation (rec {
+
+  inherit
+    pname
+    src
+    version
+    name
+    ;
 
   nativeBuildInputs = [
     nodejs
@@ -23,11 +43,7 @@ stdenv.mkDerivation (rec {
     pnpmConfigHook
   ];
 
-  pnpmDeps = fetchPnpmDeps {
-    inherit pname src version;
-    fetcherVersion = 1;
-    hash = "sha256-Oj34rQbKbsHnqPdVv+ti8z+gZTT+VOsDxg/MQ22sLRQ=";
-  };
+  pnpmDeps = if stdenv.isDarwin then darwinDeps else nixOSDeps;
 
   buildPhase = ''
     runHook preBuild
